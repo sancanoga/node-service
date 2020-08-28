60badefe78abcc9db6b594a3282d7eaea00f4455
santiago <santiago.cano@upb.edu.co>
2020-08-27 23:08:25 -0500

    add new stages pipeline to build infraestructure

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,20 @@
-resource "digitalocean_droplet""web" {
-  image              = "69142175"
+resource "digitalocean_droplet""devops_test" {
+  count              = 2
+  image              = var.image_id
   name               = "digitalocean"
   region             = "nyc3"
   size               = "1gb"
   ssh_keys           = [28221442]
+  tags               = [digitalocean_tag.devops_test.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 160 && curl ${self.ipv4_address}:3000"
+  }
+
   user_data          = <<EOF
 #cloud-config
 coreos:
@@ -20,3 +31,27 @@ coreos:
 EOF
 }
 
+resource "digitalocean_tag" "devops_test" {
+  name = "node_service"
+}
+
+resource "digitalocean_loadbalancer" "devops_test" {
+  name   = "nodeservice"
+  region = "nyc3"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 3000
+    protocol = "http"
+    path     = "/"
+  }
+
+  droplet_tag = digitalocean_tag.devops_test.name
+}
---
+++ infra/variables.tf
@@ -0,0 +1,3 @@
+variable "image_id" {
+  description = "DigitalOcean image id"
+}
